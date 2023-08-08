fd7f207c38c5d8f3bca00d2c6909e7098b3cee23
guanguancode <1271835875@qq.com>
2023-08-08 16:03:07 +0800

    新增list、detail、trash路由

---
+++ src/components/TrashDetail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="trash-detail">
+        <h1>{{ msg }}: {{ $route.params.noteId }}</h1>
+        <ul>
+            <li><router-link to="/note/1">回收站笔记本1</router-link></li>
+            <li><router-link to="/note/2">回收站笔记本2</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            msg: '回收站笔记本详情页'
+        }
+    },
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
